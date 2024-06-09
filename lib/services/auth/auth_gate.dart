ae3520b5d8abb493f7453e902156e18b79cec680
Baopvg1009 <114211379+Baopvg1009@users.noreply.github.com>
2024-06-09 18:46:56 +0700

    test

--- lib/services/auth/auth_gate.dart
+++ lib/services/auth/auth_gate.dart
@@ -1,7 +1,8 @@
-import 'package:chatapp/services/auth/login_or_register.dart';
 import 'package:chatapp/pages/home_page.dart';
+import 'package:chatapp/pages/login_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
 
@@ -10,14 +11,14 @@ class AuthGate extends StatelessWidget {
     return Scaffold(
       body: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot){
+        builder: (context, snapshot) {
           //user is logged in
-          if(snapshot.hasData){
-            return  HomePage();
+          if (snapshot.hasData) {
+            return HomePage();
           }
-          //user is not logged in
-          else{
-            return const LoginOrRegister();
+          //user is not logged in return login page
+          else {
+            return LoginPage();
           }
         },
       ),
